a93398af93b3a410d8770ef895e1b81d51f48a05
Lione313 <dilan.gutierrez@tecsup.edu.pe>
2025-04-03 16:16:35 -0500

    dadwa

--- ejercici01.dart
+++ ejercici01.dart
@@ -8,24 +8,25 @@ class Alumno {
   Alumno.primero(this.codigo, this.nombre);
 
   Alumno.segundo(this.nota1, this.nota2, this.nota3);
-  
-  
+
   @override
   String toString() {
-     double promedio = (nota1! + nota2! + nota3!) / 3;
-     return 'El alumno se llama : $nombre y su codigo es $codigo tiene notas de $nota1,$nota2,$nota3 y el             promedio de esas notas son $promedio ';
+    List<int> notas = [nota1!, nota2!, nota3!];
+    notas.sort();
+    double promedio = (notas[0] + notas[1] + notas[2]) / 3;
+    return 'El alumno se llama: $nombre, su código es $codigo, tiene notas: ${notas} y el promedio de esas notas     es $promedio';
   }
 }
 
 void main() {
   final alumno = Alumno.primero(10, 'Lionel');
-  alumno.nota1=13;
-  alumno.nota2=10;
-  alumno.nota3=20;
+  alumno.nota1 = 13;
+  alumno.nota2 = 10;
+  alumno.nota3 = 20;
   final alumno2 = Alumno.segundo(20, 18, 17);
-  alumno2.nombre='Messi';
-  alumno2.codigo=13;
-  
+  alumno2.nombre = 'Messi';
+  alumno2.codigo = 13;
+
   print(alumno.toString());
   print(alumno2.toString());
 }
